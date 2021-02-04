30933d45513ca99f6d0e919f0247d1e368fb511d
Jayashree <jshreepanda.ca@gmail.com>
2021-02-03 22:54:09 -0500

    Made the Newsletter section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Searchblock from "./components/Searchblock/Searchblock";
 import PopularDestinations from "./components/PopularDestinations/PopularDestinations"
-
+import Newsletter from "./components/Newsletter/Newsletter"
 function App() {
     return (
         <div className="App">
@@ -10,6 +10,7 @@ function App() {
             <Hero />
             <Searchblock />
             <PopularDestinations />
+            <Newsletter />
         </div>
     );
 }
---
+++ src/components/Newsletter/Newsletter.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Newsletter.scss'
+
+const Newsletter = () => {
+    return (
+        <div className="newsletter">
+            <div className="wrapper">
+                <div className="text">
+                    <h2>Subscribe Our Newsletter</h2>
+                    <p>Subscribe newsletter to get offers and about new places to discover.</p>
+                </div>
+                <div className="formnbtn">
+                    <input className='field' type="text" placeholder="Your email" />
+                    <button className="btn">Subscribe</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Newsletter
